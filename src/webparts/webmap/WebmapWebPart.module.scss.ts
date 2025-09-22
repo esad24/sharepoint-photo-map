d9f94217118a057c58d08c3915db98a8a57d7a1f
esad24 <esad.24u@gmail.com>
2025-09-22 09:58:02 +0000

    soolved scss issues

--- src/webparts/webmap/WebmapWebPart.module.scss.ts
+++ src/webparts/webmap/WebmapWebPart.module.scss.ts
@@ -1,27 +1,27 @@
 
 require("./WebmapWebPart.module.css");
 const styles = {
-  webmap: 'webmap_e60b0d0f',
-  teams: 'teams_e60b0d0f',
-  welcome: 'welcome_e60b0d0f',
-  welcomeImage: 'welcomeImage_e60b0d0f',
-  mapContainer: 'mapContainer_e60b0d0f',
-  popupImg: 'popupImg_e60b0d0f',
-  galleryContainer: 'galleryContainer_e60b0d0f',
-  photoGalleryPopup: 'photoGalleryPopup_e60b0d0f',
-  galleryNav: 'galleryNav_e60b0d0f',
-  galleryNavPrev: 'galleryNavPrev_e60b0d0f',
-  galleryNavNext: 'galleryNavNext_e60b0d0f',
-  links: 'links_e60b0d0f',
-  toast: 'toast_e60b0d0f',
-  slideUp: 'slideUp_e60b0d0f',
-  toastError: 'toastError_e60b0d0f',
-  textLabel: 'textLabel_e60b0d0f',
-  loader: 'loader_e60b0d0f',
-  loaderSpinner: 'loaderSpinner_e60b0d0f',
-  spin: 'spin_e60b0d0f',
-  loaderText: 'loaderText_e60b0d0f',
-  slideDown: 'slideDown_e60b0d0f'
+  webmap: 'webmap_99f8de0a',
+  teams: 'teams_99f8de0a',
+  welcome: 'welcome_99f8de0a',
+  welcomeImage: 'welcomeImage_99f8de0a',
+  mapContainer: 'mapContainer_99f8de0a',
+  popupImg: 'popupImg_99f8de0a',
+  galleryContainer: 'galleryContainer_99f8de0a',
+  photoGalleryPopup: 'photoGalleryPopup_99f8de0a',
+  galleryNav: 'galleryNav_99f8de0a',
+  galleryNavPrev: 'galleryNavPrev_99f8de0a',
+  galleryNavNext: 'galleryNavNext_99f8de0a',
+  links: 'links_99f8de0a',
+  toast: 'toast_99f8de0a',
+  slideUp: 'slideUp_99f8de0a',
+  toastError: 'toastError_99f8de0a',
+  textLabel: 'textLabel_99f8de0a',
+  loader: 'loader_99f8de0a',
+  loaderSpinner: 'loaderSpinner_99f8de0a',
+  spin: 'spin_99f8de0a',
+  loaderText: 'loaderText_99f8de0a',
+  slideDown: 'slideDown_99f8de0a'
 };
 
 export default styles;
